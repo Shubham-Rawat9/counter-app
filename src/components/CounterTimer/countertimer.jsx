0064bcdf99c285b2081a0ba9cd23971dab67d5d2
Shubham-Rawat9 <shubhamrawat97614@gmail.com>
2025-10-29 21:20:24 +0530

    folder & componets create

--- src/components/CounterTimer/countertimer.jsx
+++ src/components/CounterTimer/countertimer.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const CountTime = () => {
+
+  const [ time ,setTime] = useState(0)
+
+const handleInput =(e) =>{
+  setTime(parseInt(e.target.value * 60));
+}
+const formatTime = ()=>{
+  const min= String(Math.floor(time/60)).padStart(2,'0');
+  const sec = String(time%60).padStart(2,'0');
+  return `${min} :${sec}`
+}
   return (
     <>
-        <h4>conter time componets</h4>
+        <div className='countdown-timer'>
+        <h1>Contdown Timer</h1>
+        <div className='timer-display'>
+
+          <input  onChange={handleInput} type="number"  placeholder='Enter Time In Minutes'/>
+
+          <div>{formatTime()}</div>
+
+          <div className='timer-controls'>
+
+            <button>Start</button>
+            <button>Pause</button>
+            <button>Reset</button>
+
+          </div>
+        </div>
+
+        </div>
 
     </> 
   )  }
