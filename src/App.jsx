8ad937aa4577b37524b434ea8b3a9c28dc17d72b
Shubham-Rawat9 <shubhamrawat97614@gmail.com>
2025-10-29 21:02:04 +0530

    counter project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import CountTime from "./components/CounterTimer/countertimer"
+
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <CountTime/>
+      </div>
+     </>
+  )
+}
+
+export default App
---
+++ src/components/CounterTimer/countertimer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CountTime = () => {
+  return (
+    <>
+        <h4>conter time componets</h4>
+
+    </> 
+  )  }
+
+    export default CountTime 
